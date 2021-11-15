f285568f593aaaeb2f4b8ac58e4758abb524e5cc
Teo <teo.lairla@iessierradeguara.com>
2021-11-15 10:31:46 +0100

    añadido try catch

--- hilos02-ContadorPalabras/src/ContadorPalabras.java
+++ hilos02-ContadorPalabras/src/ContadorPalabras.java
@@ -4,19 +4,26 @@ import java.util.Scanner;
 
 public class ContadorPalabras {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args){
 		String rutaFichero = "/home/alumno/texto.txt";
 		if (args.length > 0) {
 			rutaFichero = args[0];
 		}
-		Scanner lector = new Scanner(new File(rutaFichero));
-		int contador = 0;
+		Scanner lector;
+		try {
+			lector = new Scanner(new File(rutaFichero));
+			int contador = 0;
 
-		while (lector.hasNext()) {
-			lector.next();
-			contador++;
+			while (lector.hasNext()) {
+				lector.next();
+				contador++;
+			}
+			System.out.println("Total palabras: "+contador+".");
+		} catch (FileNotFoundException e) {
+			System.err.println("Fichero "+rutaFichero+" no encontrado.");
+			e.printStackTrace();
 		}
-		System.out.println("Total palabras: "+contador+".");
+		
 
 	}
 
