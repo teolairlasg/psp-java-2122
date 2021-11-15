0db1f14fc8324cfc1269187b03ea5a07bec141a0
Teo <teo.lairla@iessierradeguara.com>
2021-11-15 10:22:11 +0100

    Añadida clase que ejecuta el main de otra clase
    La clase añadida es EjecutaPrograma.

---
+++ hilos01/src/hilos01/EjecutaPrograma.java
@@ -0,0 +1,15 @@
+package hilos01;
+
+public class EjecutaPrograma {
+
+	public static void main(String[] args) {
+		try {
+			Programa.main(null);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
